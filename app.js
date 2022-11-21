ff65dd9fd47cdebc7e2699b0376c81cb60caf959
fadhlur497 <fadhlurrahman497@gmail.com>
2022-11-21 23:38:31 +0700

    deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const Controller = require('./controller/controller')
 const routes = require('./routers/index')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.set('view engine','ejs')
 app.use(express.urlencoded({ extended:false}))
